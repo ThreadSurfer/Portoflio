f9a79fb5d777fc3fc4cc9cc9d646c0d1aedb6471
Joel Giladi <joelgiladi@Joels-MacBook-Pro.local>
2021-07-06 01:01:27 -0700

    still fixing port

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use('/images', express.static('images'));
 
 app.set('view engine', 'pug');
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/images', (req, res) => {
     console.log(dirname);
 })
@@ -31,7 +33,3 @@ app.get('/layout', (req, res) => {
 app.get('/project/:projectId', (req, res) => {
     res.render('project', {projectId: req.params.projectId, data: data.projects}); //This will send the projectId as a param when the user clicks on a project.
 });
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('App running on port 3000');
-})
